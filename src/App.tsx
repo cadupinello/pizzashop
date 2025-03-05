359eafb10173086f19625a2423f0f7c1f893e99d
carlos.rodrigues <carlos.rodrigues@digitalk.com.br>
2025-03-05 19:59:54 -0300

    init project, config default

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <Button variant="default">Button</Button>
+      </div>
+    </>
+  )
+}
